55dca442d6cac28b6169bbd38d25f9238538a294
Xeon-243859 <rsm5114@naver.com>
2025-04-24 17:48:27 +0900

    🎉 Git 연결 완료 및 초기 커밋

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import IntroPage from './IntroPage';
+import EmotionPage from './EmotionPage';
+import MessagePage from './MessagePage';
+import PreviewPage from './PreviewPage';
+import VideoPage from './VideoPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/emotion" element={<EmotionPage />} />
+        <Route path="/message" element={<MessagePage />} />
+        <Route path="/preview" element={<PreviewPage />} />
+        <Route path="/video" element={<VideoPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
