5c9cd46b75a969cc2b948ef4388f5559aaf15936
Xeon-243859 <rsm5114@naver.com>
2025-04-24 18:35:57 +0900

    💾 누락된 페이지 파일 구조 및 라우팅 전체 반영

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import IntroPage from './IntroPage';
 import EmotionPage from './EmotionPage';
 import MessagePage from './MessagePage';
-import PreviewPage from './PreviewPage';
 import VideoPage from './VideoPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,7 +15,6 @@ root.render(
         <Route path="/" element={<IntroPage />} />
         <Route path="/emotion" element={<EmotionPage />} />
         <Route path="/message" element={<MessagePage />} />
-        <Route path="/preview" element={<PreviewPage />} />
         <Route path="/video" element={<VideoPage />} />
       </Routes>
     </BrowserRouter>
