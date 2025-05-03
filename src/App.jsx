c40c99c784e09270780938da61974b6938bc6cc6
Xeon-243859 <rsm5114@naver.com>
2025-05-03 17:57:43 +0900

    🐛 fix: App.jsx 라우터 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import IntroPage from "./IntroPage";
+import HomePage from "./pages/HomePage";
 import LovePreviewPage from "./pages/LovePreviewPage";
+import MessagePage from "./pages/MessagePage";
+import WelcomePage from "./pages/WelcomePage";
+import EmotionPage from "./pages/EmotionPage";
+import LoveConfessionPage from "./pages/LoveConfessionPage";
+import LoveWritePage from "./pages/LoveWritePage";
+import LoveFinalPage from "./pages/LoveFinalPage";
 import TestAudioPage from "./pages/TestAudioPage";
+
+import "./styles/LovePreviewPage.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <div style={{ padding: "2rem", textAlign: "center" }}>
-            <h1>뿅!톡 메인 페이지</h1>
-            <ul>
-              <li><a href="/love/preview">사랑 고백 템플릿</a></li>
-              <li><a href="/test-audio">🎵 음악 테스트 페이지</a></li>
-            </ul>
-          </div>
-        } />
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/love/preview" element={<LovePreviewPage />} />
+        <Route path="/message" element={<MessagePage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/emotion" element={<EmotionPage />} />
+        <Route path="/love/write" element={<LoveWritePage />} />
+        <Route path="/love/confess" element={<LoveConfessionPage />} />
+        <Route path="/love/final" element={<LoveFinalPage />} />
         <Route path="/test-audio" element={<TestAudioPage />} />
       </Routes>
     </Router>
