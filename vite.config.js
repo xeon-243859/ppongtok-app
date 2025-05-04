60dcf2a31ad71425efc3f93e710b884efccf4789
Xeon-243859 <rsm5114@naver.com>
2025-05-04 12:15:33 +0900

    ✅ config 파일 재정리

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Vercel, GitHub Pages 배포 시 필요
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
 });
