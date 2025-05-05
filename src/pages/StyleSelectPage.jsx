771693e5ec76959e7d824dc344a12f96adfda7f3
Xeon-243859 <rsm5114@naver.com>
2025-05-05 17:52:47 +0900

    📝 문장 수정 및 전체 중앙 정렬 완료

--- src/pages/StyleSelectPage.jsx
+++ src/pages/StyleSelectPage.jsx
@@ -19,15 +19,17 @@ const StyleSelectPage = () => {
   };
 
   return (
-    <div className="style-select-container">
-      <h2 className="title-text">
-        어떤 배경으로<br />사랑을 담아볼까?
-      </h2>
-      <div className="select-buttons">
-        <button onClick={goToImageSelect}>🖼 이미지 배경</button>
-        <button onClick={goToVideoSelect}>🎞 영상 배경</button>
+    <div className="style-select-wrapper">
+      <div className="style-select-container">
+        <h2 className="title-text">
+          어떤 배경으로<br />사랑을 담아볼까요?
+        </h2>
+        <div className="select-buttons">
+          <button onClick={goToImageSelect}>🖼 이미지 배경</button>
+          <button onClick={goToVideoSelect}>🎞 영상 배경</button>
+        </div>
+        <button className="back-button" onClick={goBack}>뒤로가기</button>
       </div>
-      <button className="back-button" onClick={goBack}>뒤로가기</button>
     </div>
   );
 };
