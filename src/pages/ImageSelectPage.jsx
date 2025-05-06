9f369b4ad068c1c747bb7dc7292e6272735fc11e
Xeon-243859 <rsm5114@naver.com>
2025-05-06 16:44:02 +0900

    ✅ CSS import 경로 대소문자 수정 및 빌드 오류 해결

--- src/pages/ImageSelectPage.jsx
+++ src/pages/ImageSelectPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../ImageSelectPage.css";
+import "./ImageSelectPage.css";
 
 const images = [
   "/backgrounds/cosmos.jpg",
