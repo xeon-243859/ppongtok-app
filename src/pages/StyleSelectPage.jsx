ce0aee5b1ca10f98e724837379857034bbc9e97a
Xeon-243859 <rsm5114@naver.com>
2025-05-09 22:21:28 +0900

    Test: 자막 하단 정중앙 타자체 정렬 실험

--- src/pages/StyleSelectPage.jsx
+++ src/pages/StyleSelectPage.jsx
@@ -3,8 +3,10 @@ import '../styles/StyleSelectPage.css';
 
 function StyleSelectPage() {
   return (
-    <div className="bottom-wrapper">
-      <h2 className="typing-text">어떤 배경으로 사랑을 담아볼까요?</h2>
+    <div className="root-wrapper">
+      <div className="bottom-wrapper">
+        <h2 className="typing-text">어떤 배경으로 사랑을 담아볼까요?</h2>
+      </div>
     </div>
   );
 }
