47ba4de1c5dc51afe0facb0ecf44942153a03be7
Xeon-243859 <rsm5114@naver.com>
2025-05-09 22:27:18 +0900

    Fix: 메시지 입력 화면 구조 기반 자막 위치 정중앙 타자체 구현

--- src/pages/StyleSelectPage.jsx
+++ src/pages/StyleSelectPage.jsx
@@ -3,10 +3,8 @@ import '../styles/StyleSelectPage.css';
 
 function StyleSelectPage() {
   return (
-    <div className="root-wrapper">
-      <div className="bottom-wrapper">
-        <h2 className="typing-text">어떤 배경으로 사랑을 담아볼까요?</h2>
-      </div>
+    <div className="message-container">
+      <h2 className="typing-text">어떤 배경으로 사랑을 담아볼까요?</h2>
     </div>
   );
 }
