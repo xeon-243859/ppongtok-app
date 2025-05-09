7b5a88785b29dffdb1bce4e1b9a46a6317ed5a2b
Xeon-243859 <rsm5114@naver.com>
2025-05-09 22:08:18 +0900

    Feat: 타자체 자막 정중앙 위치 구현

--- src/pages/StyleSelectPage.jsx
+++ src/pages/StyleSelectPage.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../styles/StyleSelectPage.css';
 
 function StyleSelectPage() {
-  const navigate = useNavigate();
-
   return (
-    <div className="love-select-wrapper">
-      <h2 className="love-title">어떤 배경으로 사랑을 담아볼까요?</h2>
-
-      <div className="love-button-group">
-        <button className="love-button" onClick={() => navigate('/image/select')}>
-          이미지 배경 선택
-        </button>
-        <button className="love-button" onClick={() => navigate('/video/select')}>
-          영상 배경 선택
-        </button>
-      </div>
+    <div className="title-container">
+      <h2 className="typing-text">어떤 배경으로 사랑을 담아볼까요?</h2>
     </div>
   );
 }
