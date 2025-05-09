a814963740a5f2e42a3bd0f52694d9a1df2eace6
Xeon-243859 <rsm5114@naver.com>
2025-05-09 14:30:11 +0900

    🚪 라우팅 수정: 앱 시작 시 /intro로 이동하도록 초기 진입 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
+// ✅ App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
+import IntroPage from "./pages/IntroPage";
 import VideoEntryPage from "./pages/VideoEntryPage";
 import VideoSelectPage from "./pages/VideoSelectPage";
-import VideoUploadPage from "./pages/VideoUploadPage"; // 만약 만들 예정이라면
+import VideoUploadPage from "./pages/VideoUploadPage";
+import MusicSelectPage from "./pages/MusicSelectPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 시작 시 무조건 entry 페이지로 */}
-        <Route path="/" element={<Navigate to="/video/entry" replace />} />
-        
-        {/* 영상 선택 진입화면 (1단계) */}
+        {/* 앱 시작 시 intro로 리디렉트 */}
+        <Route path="/" element={<Navigate to="/intro" />} />
+        <Route path="/intro" element={<IntroPage />} />
         <Route path="/video/entry" element={<VideoEntryPage />} />
-
-        {/* 영상 저장소 선택화면 (썸네일) */}
         <Route path="/video/select" element={<VideoSelectPage />} />
-
-        {/* 내 파일 업로드 선택화면 */}
         <Route path="/video/upload" element={<VideoUploadPage />} />
+        <Route path="/music/select" element={<MusicSelectPage />} />
       </Routes>
     </Router>
   );
