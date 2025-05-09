01abe83a23c51f8957416e7b744486ab5a9ec30a
Xeon-243859 <rsm5114@naver.com>
2025-05-09 22:52:16 +0900

    Fix: main.jsx에 BrowserRouter 적용 및 불필요한 JSX 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ 라우터 import
 import App from "./App.jsx";
 import "./index.css";
-<Route path="/love/style" element={<StyleSelectPage />} />
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* ✅ App을 BrowserRouter로 감싸서 라우팅 작동 */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
