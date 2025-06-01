1bca031e6637c1e5f3e585bd24842695036358af
Xeon-243859 <rsm5114@naver.com>
2025-06-01 23:30:54 +0900

    🔧 Fix: Update PreviewPage styles and replace video files

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -134,7 +134,7 @@ const PreviewPage = () => {
               fontSize: "18px",
               fontWeight: "bold",
               color: "white",
-              animation: "marquee 60s linear forwards",
+              animation: "marquee 15s linear forwards",
             }}
           >
             {repeatedMessage}
