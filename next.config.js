6fcd4ffa4db159552a290888266957d7cd85b92a
Xeon-243859 <rsm5114@naver.com>
2025-06-08 23:02:44 +0900

    Final clean build for /intro fix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
+   // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["firebasestorage.googleapis.com"],
-  },
-
-   async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/intro',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
