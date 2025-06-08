b00f2045dd8096ded6ed38c13507a6de2f23c063
Xeon-243859 <rsm5114@naver.com>
2025-06-08 16:17:46 +0900

    router.replace change

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default function IndexPage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/intro"); // 또는 앱의 메인 경로
+    router.replace("/intropage"); // 또는 앱의 메인 경로
   }, []);
 
   return null;
