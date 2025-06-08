07ecd1bc4e69a9d71b38ca526ffa8dd91f540ab7
Xeon-243859 <rsm5114@naver.com>
2025-06-09 00:36:32 +0900

    ✅ CSS 경로 수정으로 빌드 오류 해결

--- pages/write-message.jsx
+++ pages/write-message.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import styles from "../styles/write-message.module.css"; 
+import styles from "../src/styles/write-message.module.css";
 
 
 export default function WriteMessagePage() {
