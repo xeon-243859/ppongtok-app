19de2c8c28953024f79dfed3f228301cdee72eb5
Xeon-243859 <rsm5114@naver.com>
2025-06-09 12:25:20 +0900

    Rename all files to lowercase for case-sensitive deployment

--- pages/TestAudioPage.jsx
+++ pages/TestAudioPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router"; // ✅ useNavigate → useRouter
-import styles from "./StyleSelectPage.module.css";
+import styles from "./style-select.module.css";
 
 export default function TestAudioPage() {
   const router = useRouter();
