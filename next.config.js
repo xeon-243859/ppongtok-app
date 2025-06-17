e884c9b6aae74ea0cd70f3f19bed87e33d96bf6b
Xeon-243859 <rsm5114@naver.com>
2025-06-18 03:31:13 +0900

    next.config.js 코드변경저장

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-   // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // ✅ SSR용 빌드 유지
+  experimental: {
+    appDir: false
+  }
 };
 
 module.exports = nextConfig;
