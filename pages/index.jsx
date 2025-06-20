a07271630c732c2f5a71e9247b2f923e425aacf8
Xeon-243859 <rsm5114@naver.com>
2025-06-21 04:26:18 +0900

    index.jsx 경로수정

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,8 @@ import Head from 'next/head';
 import { useState } from 'react';
 // ShareButton 컴포넌트의 실제 경로를 확인해주세요.
 
-import ShareButton from '.. /src/components/sharebutton';
+
+import ShareButton from '../src/components/ShareButton';
 import appStyles from '../src/styles/AppTheme.module.css';
 
 export default function Home() {
