83d67164791031b7eda70c9db50eb50e0588dcd6
Xeon-243859 <rsm5114@naver.com>
2025-06-22 00:16:34 +0900

    대소문자 수정

--- pages/musicselectpage.jsx
+++ pages/musicselectpage.jsx
@@ -4,7 +4,7 @@ import React, { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import imageCompression from "browser-image-compression";
 import { TypeAnimation } from "react-type-animation";
-import pageStyles from "../src/styles/ImageSelectPage.module.css"; 
+import pageStyles from "../src/styles/imageselectpage.module.css"; 
 
 export default function ImageSelectPage() {
   const router = useRouter();
